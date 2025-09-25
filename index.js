7d5a6d6f31323c81a753edb4ef647b963ba3515e
Ashan Kavinda <bandaralamak.23@uom.lk>
2025-09-25 19:26:31 +0530

    ok

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require("express");
 require("dotenv").config();
 require("./config/db");
 const app = express();  
-const PORT = process.env.PORT;
-
-
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+const patientCollection = require("./model/patient");
+
+app.post("/send-patient", async (req, res) => {
+  try {
+    const patientData = req.body;
+    const newPatient = new patientCollection(patientData);
+    await newPatient.save();
+    res.status(200).send({ message: "Patient data saved successfully", data: newPatient });
+  } catch (error) {
+    res.status(500).send({ message: "Error saving patient data", error: error.message });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- model/patient.js
+++ model/patient.js
@@ -1,3 +1,31 @@
 const mongoose = require("mongoose");
 
 const db = mongoose.connection.useDb("Hospital");
+
+const patientSchema = new mongoose.Schema({
+  Name: {
+    type: String,
+  },
+  Age: {
+    type: Number,
+  },
+  Gender: {
+    type: String,
+  },
+Email:{
+    type: String,
+},
+  Address: {
+    type: String,
+  },
+  Phone: {
+    type: String,
+  },
+    Disease: {
+    type: String,
+  },
+});
+
+const Patient = db.model("Patient", patientSchema);
+
+module.exports = Patient;
